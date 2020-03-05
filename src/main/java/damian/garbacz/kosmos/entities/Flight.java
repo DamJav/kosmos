f34208bfcd92cbeed69963df6a01d023e2d09695
DamJav <damian.garbacz.9@gmail.com>
2020-03-05 16:35:15 +0100

    encje cd..

--- src/main/java/damian/garbacz/kosmos/entities/Flight.java
+++ src/main/java/damian/garbacz/kosmos/entities/Flight.java
@@ -1,4 +1,22 @@
 package damian.garbacz.kosmos.entities;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "flights")
+@Getter
+@Setter
 public class Flight {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
 }
--- src/main/java/damian/garbacz/kosmos/entities/Tourist.java
+++ src/main/java/damian/garbacz/kosmos/entities/Tourist.java
@@ -1,4 +1,21 @@
 package damian.garbacz.kosmos.entities;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tourists")
+@Getter
+@Setter
 public class Tourist {
+
+    @Id
+    @GeneratedValue
+    private Long id;
 }
