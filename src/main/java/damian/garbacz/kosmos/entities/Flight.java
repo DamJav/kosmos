a39e815333b8aaf0b6fa1fa15b4a78c6b88871b9
DamJav <damian.garbacz.9@gmail.com>
2020-03-05 17:26:14 +0100

    ENcja lotu skonczona

--- src/main/java/damian/garbacz/kosmos/entities/Flight.java
+++ src/main/java/damian/garbacz/kosmos/entities/Flight.java
@@ -4,11 +4,10 @@ package damian.garbacz.kosmos.entities;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "flights")
@@ -19,6 +18,11 @@ public class Flight {
     @Id
     @GeneratedValue
     private Long id;
-
+    private LocalDateTime arrivalTime;
+    private LocalDateTime departureTime;
+    private Integer seatsCount;
+    @ManyToMany
+    private List<Tourist> tourists;
+    private Integer ticketPrice;
 
 }
