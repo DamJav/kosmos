af8845ffb59e8f0061dd3877259eaa7fd75602d5
DamJav <damian.garbacz.9@gmail.com>
2020-03-05 15:59:05 +0100

    Startspring

---
+++ src/main/java/damian/garbacz/kosmos/KosmosApplication.java
@@ -0,0 +1,13 @@
+package damian.garbacz.kosmos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosmosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KosmosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/damian/garbacz/kosmos/KosmosApplicationTests.java
@@ -0,0 +1,13 @@
+package damian.garbacz.kosmos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KosmosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
