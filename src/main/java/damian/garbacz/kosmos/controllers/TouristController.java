4cfadee14b26e0dcc9aaed8d8a34b3d4e4ec60c2
DamJav <damian.garbacz.9@gmail.com>
2020-03-07 10:31:38 +0100

    tourist controller

--- src/main/java/damian/garbacz/kosmos/controllers/TouristController.java
+++ src/main/java/damian/garbacz/kosmos/controllers/TouristController.java
@@ -1,10 +1,24 @@
 package damian.garbacz.kosmos.controllers;
 
 
+import damian.garbacz.kosmos.services.TouristService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/tourist")
+@RequestMapping("/tourists")
 public class TouristController {
+
+    private final TouristService touristService;
+
+    public TouristController(TouristService touristService) {
+        this.touristService = touristService;
+    }
+
+
+    @GetMapping("")
+    public String showTourists(){
+        return "tourists";
+    }
 }
