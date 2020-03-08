976dcfa585e2afe3a8ab98d918c874c3a99967f1
DamJav <damian.garbacz.9@gmail.com>
2020-03-08 17:19:35 +0100

    lista turystów - repo

--- src/main/java/damian/garbacz/kosmos/repositories/TouristRepository.java
+++ src/main/java/damian/garbacz/kosmos/repositories/TouristRepository.java
@@ -4,6 +4,10 @@ import damian.garbacz.kosmos.entities.Tourist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TouristRepository extends JpaRepository<Tourist, Long> {
+
+    List<Tourist> findAll();
 }
--- src/main/java/damian/garbacz/kosmos/services/impl/DefaultTouristService.java
+++ src/main/java/damian/garbacz/kosmos/services/impl/DefaultTouristService.java
@@ -1,11 +1,19 @@
 package damian.garbacz.kosmos.services.impl;
 
 import damian.garbacz.kosmos.entities.Tourist;
+import damian.garbacz.kosmos.repositories.TouristRepository;
 import damian.garbacz.kosmos.services.TouristService;
 
 import java.util.List;
 
 public class DefaultTouristService implements TouristService {
+
+    private final TouristRepository touristRepository;
+
+    public DefaultTouristService(TouristRepository touristRepository) {
+        this.touristRepository = touristRepository;
+    }
+
     @Override
     public List<Tourist> findAllTourists() {
 
